831133559cd4374af10d9b8299496f3880d995d4
Arthur <artur.muchliado@gmail.com>
2019-09-17 18:24:48 +0300

    update

--- Java_Advanced_Homework/src/JavaAdvanced_homework_GameOfLife_02/GameOfLife.java
+++ Java_Advanced_Homework/src/JavaAdvanced_homework_GameOfLife_02/GameOfLife.java
@@ -15,25 +15,15 @@ public class GameOfLife {
 		
 		while(true) {
 		System.out.println("Generation: " + (generation++));
-		printGrid(grid); // printing Grid.
+		printGrid(grid); 
 		System.out.println();
 		
-		String[][] superGrid = copyOfGrid(grid);    // superGrid will be a copy of a grid.  
+		String[][] superGrid = copyOfGrid(grid);    
 		
-		for (int i = 0; i < grid.length; i++) { // a loop through first printed grid. Same while iteration. 
-            for (int j = 0; j < grid[i].length; j++) { // looping through each cell, takes one cell and check it's neighbours, and push them to a list. 
+		for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) { 
             	
-                ArrayList<String> neighbours = new ArrayList<String>(); // creating a list, where we put all neighbours of each cell. 
-                
-                // This don't work because of edges. 
-//                neighbors.add(grid[i - 1][j - 1]);
-//                neighbors.add(grid[i - 1][j]);
-//                neighbors.add(grid[i - 1][j + 1]);
-//                neighbors.add(grid[i][j - 1]);
-//                neighbors.add(grid[i][j + 1]);
-//                neighbors.add(grid[i + 1][j - 1]);
-//                neighbors.add(grid[i + 1][j]);
-//                neighbors.add(grid[i + 1][j + 1]);
+                ArrayList<String> neighbours = new ArrayList<String>(); 
                 
                 // Find the neighbour cells of the current cell.
                 // Modulus solve the edge problem. Out of bounds neighbour is the other side neighbour.
@@ -47,31 +37,30 @@ public class GameOfLife {
                 neighbours.add(grid[Math.floorMod(i + 1, grid.length)][Math.floorMod(j + 1, grid[i].length)]);
                 
                 int life_forms = 0;
-                for (int k = 0; k < neighbours.size(); k++) { // looping through neighbours of a current cell. 
-                    if (neighbours.get(k).compareTo(LIFE) == 0) // if current neighbour equals to LIFE, count it ++;
-                        life_forms += 1; // counting LIFE neighbours
+                for (int k = 0; k < neighbours.size(); k++) { 
+                    if (neighbours.get(k).compareTo(LIFE) == 0) 
+                        life_forms += 1; 
                 }
                 
-                // Implementing Game Of Life rules.
-                if (grid[i][j].compareTo(DEATH) == 0) { // checking each cell of current grid if that cell is DEATH
-                    if (life_forms == 3) { // and if it's DEATH and have 3 LIFE neighbours 
-                        superGrid[i][j] = LIFE; // change this cell to LIFE in a new second superGrid that will be printed next.
+                if (grid[i][j].compareTo(DEATH) == 0) { 
+                    if (life_forms == 3) { 
+                        superGrid[i][j] = LIFE; 
                     }
-                } else { // else current cell stays the same.
-                    // check if cell must die of over-population or under-population
+                } else { 
                     if (life_forms < 2 || life_forms > 3) {
-                        superGrid[i][j] = DEATH; // change this cell to DEATH in a new second superGrid that will be printed next.
+                        superGrid[i][j] = DEATH;
                     }
                 }
             }
         }
+		
 		// Checking previous and next arrays, if equal - iteration stopped.
 		if (Arrays.deepEquals(grid, superGrid)) {
 			System.out.println("Second grid is the same as previous, iteration is finished");
 			printGrid(superGrid);
          	break;
          } 
-        // In the end of iteration grid is equal to a new formed grid, and printed now again at the top 'printGrid(grid)'
+      
         grid = copyOfGrid(superGrid); 
         
 		try {
@@ -107,7 +96,7 @@ public class GameOfLife {
 	 * Prints existing grid. 
 	 * @param matrix
 	 */
-	public static void printGrid(String[][] matrix) {
+	public static String[][] printGrid(String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(" " + matrix[i][j] + " ");
@@ -115,6 +104,7 @@ public class GameOfLife {
 
             System.out.println();
         }
+        return matrix;
     }
 	
 	/**
